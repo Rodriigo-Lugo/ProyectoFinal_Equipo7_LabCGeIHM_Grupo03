cea10eee3c869c668b94b46553fedc7c2ec9a249
Rodrigo_Lugo <rodrigo.lugofi@gmail.com>
2025-11-05 22:34:56 -0600

    arreglar camara

--- Practica7x64/practica10keyframes.cpp
+++ Practica7x64/practica10keyframes.cpp
@@ -626,7 +626,7 @@ int main()
 		
 		glUniform2fv(uniformTextureOffset, 1, glm::value_ptr(toffset));
 		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, -7000.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
